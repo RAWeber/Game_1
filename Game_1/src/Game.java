3f0c36225c2d4f1601ba81949f5ffb19fa8bce0d
Robert Weber <RAWeber95@gmail.com>
2015-05-15 14:52:56 -0700

    Where you go

--- Game_1/src/Game.java
+++ Game_1/src/Game.java
@@ -2,7 +2,7 @@
 public class Game {
 	public static void main(String args[]){
 		System.out.print("Hello World");
-		System.out.print("Games");
-		System.out.print("Tests");
+		System.out.println("Games");
+		System.out.println("Tests");
 	}
 }
