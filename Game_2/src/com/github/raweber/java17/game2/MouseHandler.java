d25ae968dc4eb560e4ff84522be4d0e4fed6bbde
Robert Weber <RAWeber95@gmail.com>
2015-05-27 01:02:22 -0700

    Added range, fixed grid boundries

--- Game_2/src/com/github/raweber/java17/game2/MouseHandler.java
+++ Game_2/src/com/github/raweber/java17/game2/MouseHandler.java
@@ -34,7 +34,6 @@ public class MouseHandler {
 								if(e.getX()>=(int)(Screen.TOWER_WIDTH*6+(x*Screen.TOWER_WIDTH)) && e.getX()<=(int)(Screen.TOWER_WIDTH*6+(x*Screen.TOWER_WIDTH))+Screen.TOWER_WIDTH){
 									if(e.getY()>=Screen.TOWER_HEIGHT*17+(y*Screen.TOWER_HEIGHT) && e.getY()<=Screen.TOWER_HEIGHT*19+(y*Screen.TOWER_HEIGHT)){
 										if(Screen.player.getMoney()>=TowerStore.towers[x*2+y].getCost()){
-											System.out.println("You bought a "+TowerStore.towers[x*2+y]+" for "+TowerStore.towers[x*2+y].getCost()+" coins!");
 											holding=x*2+y+1;
 										}
 									}
@@ -48,14 +47,16 @@ public class MouseHandler {
 	}
 	
 	public static void placeTower(int x, int y){
-		int xPos=(x-Screen.TOWER_WIDTH)/Screen.TOWER_WIDTH;
-		int yPos=(y-Screen.TOWER_HEIGHT)/Screen.TOWER_HEIGHT;
+		int xPos=x/Screen.TOWER_WIDTH-1;
+		int yPos=y/Screen.TOWER_HEIGHT-1;
 		
-		if(xPos>25 || yPos>15){
-			
-		}else if(Screen.towerMap[xPos][yPos]==null && Screen.map[xPos][yPos]==0){
-			Screen.player.setMoney(Screen.player.getMoney()-TowerStore.towers[holding-1].getCost());
-			Screen.towerMap[xPos][yPos]=TowerStore.towers[MouseHandler.holding-1];
+		if(xPos>=0 && xPos<25 && yPos>=0 && yPos<15){
+
+			if(Screen.towerMap[xPos][yPos]==null && Screen.map[xPos][yPos]==0){
+				Screen.player.setMoney(Screen.player.getMoney()-TowerStore.towers[holding-1].getCost());
+				System.out.println("You bought a "+TowerStore.towers[holding-1]+" for "+TowerStore.towers[holding-1].getCost()+" coins!");
+				Screen.towerMap[xPos][yPos]=TowerStore.towers[MouseHandler.holding-1];
+			}
 		}
 	}
 
